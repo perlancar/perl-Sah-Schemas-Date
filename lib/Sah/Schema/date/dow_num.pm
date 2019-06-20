96399cf9cd7371ebf81b9741d58c6921fe970fa0
perlancar (on pc-office) <perlancar@gmail.com>
2019-06-20 19:47:08 +0700

    Release v0.002

--- lib/Sah/Schema/date/dow_num.pm
+++ lib/Sah/Schema/date/dow_num.pm
@@ -4,7 +4,7 @@ package Sah::Schema::date::dow_num;
 # VERSION
 
 our $schema = [int => {
-    summary => 'Day-of-week number (1-7, 1=Sunday)',
+    summary => 'Day-of-week number (1-7, 1=Monday)',
     min => 1,
     max => 7,
     'x.perl.coerce_rules' => ['str_convert_en_dow_name_to_num'],
