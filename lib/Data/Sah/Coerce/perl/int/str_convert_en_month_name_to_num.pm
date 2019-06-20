a069a50cafeaacf3d07566111b24e7a671a133d7
perlancar (on pc-office) <perlancar@gmail.com>
2019-06-20 19:16:20 +0700

    Add coerce rule for month_num: str_convert_en_month_name_to_num (from English month name)

---
+++ lib/Data/Sah/Coerce/perl/int/str_convert_en_month_name_to_num.pm
@@ -0,0 +1,67 @@
+package Data::Sah::Coerce::perl::int::str_convert_en_month_name_to_num;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 4,
+        prio => 50,
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+    my $pkg = __PACKAGE__;
+
+    $res->{expr_match} = "!ref($dt)";
+    $res->{expr_coerce} = join(
+        "",
+        "do { ",
+
+        # since this is a small translation table we put it inline, but for
+        # larger translation table we should move it to a separate perl module
+        "\$$pkg\::month_nums ||= {",
+        "  jan=>1, january=>1, ",
+        "  feb=>2, february=>2, ",
+        "  mar=>3, march=>3, ",
+        "  apr=>4, april=>4, ",
+        "  may=>5, ",
+        "  jun=>6, june=>6, ",
+        "  jul=>7, july=>7, ",
+        "  aug=>8, august=>8, ",
+        "  sep=>9, sept=>9, september=>9, ",
+        "  oct=>10, october=>10, ",
+        "  nov=>11, november=>11, ",
+        "  dec=>12, december=>12, ",
+        "}; ",
+        "\$$pkg\::month_nums->{lc $dt} || $dt; ",
+        "}",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT: Convert English month name (e.g. Dec, april) to number (1-12)
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
+
+This rule can convert English month names like:
+
+ Jan
+ april
+ JUN
+
+to corresponding month numbers (i.e. 1, 4, 6 in the examples above).
+Unrecognized strings will just be passed as-is.
--- lib/Sah/Schema/date/month_num.pm
+++ lib/Sah/Schema/date/month_num.pm
@@ -7,6 +7,7 @@ our $schema = [int => {
     summary => 'Month number',
     min => 1,
     max => 12,
+    'x.perl.coerce_rules' => ['str_convert_en_month_name_to_num'],
 }, {}];
 
 1;
